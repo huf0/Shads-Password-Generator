cfa5542fa641091b153117ea8abf5ff4e9cd6d5c
huf <epic1102@icloud.com>
2021-10-05 20:17:20 -0400

    added if statements to see any users choice

--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -1,9 +1,94 @@
 // Assignment code here
 function generatePassword() {
-  var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseSet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var upperCaseSet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
   var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+
+  var specialSet = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "+",
+    "?",
+    "/",
+    "-",
+    ":",
+    ";",
+    "[",
+    "]",
+    "{",
+    "}",
+    ".",
+    "<",
+    ">",
+    "=",
+    "_",
+    "`",
+    "|",
+    "~",
+  ];
+  
   var selectedArray = [];
 
   var passwordLength = getPasswordLength();
@@ -15,16 +100,30 @@ function generatePassword() {
     var upperCase = getChoice("uppercase");
     var numericCharacters = getChoice("numeric");
     var specialCharacters = getChoice("special");
-    if ((lowerCase) || (upperCase) || (numericCharacters) || (specialCharacters)) {
+    if (lowerCase || upperCase || numericCharacters || specialCharacters) {
       charTypeSelected = true;
     } else {
-      window.alert("You must select at least one character type.")
+      window.alert("You must select at least one character type.");
     }
   }
+}
 
-  
+if (lowerCase) {
+  selectedArray = selectedArray.concat(lowerCaseSet);
+}
+if (upperCase) {
+  selectedArray = selectedArray.concat(upperCaseSet);
+}
+if (numericCharacters) {
+  selectedArray = selectedArray.concat(numSet);
+}
+if (specialCharacters) {
+  selectedArray = selectedArray.concat(specialSet);
+}
 
-  // Get references to the #generate element
+var passwordString = "";
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -33,7 +132,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
